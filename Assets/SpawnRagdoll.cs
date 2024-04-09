ef281d815b79fbfc89e76c4feeba0a052c1b23dd
KWadas <wadaskyle03@gmail.com>
2024-04-08 23:14:09 -0600

    ALMOST FINISHED

--- Assets/SpawnRagdoll.cs
+++ Assets/SpawnRagdoll.cs
@@ -6,14 +6,17 @@ public class SpawnRagdoll : MonoBehaviour
 {
     int count = 0;
     [SerializeField] GameObject ragdoll;
+    [SerializeField] CapsuleCollider capsule;
+    [SerializeField] GameObject mesh;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (count < 1 && collision.gameObject.tag == "Player")
         {
             count++;
-            Instantiate(ragdoll, transform.position, transform.rotation, transform);
-            gameObject.SetActive(false);
+            Instantiate(ragdoll, transform.position, transform.rotation);
+            capsule.gameObject.SetActive(false);
+            mesh.SetActive(false);
         }
     }
 }
